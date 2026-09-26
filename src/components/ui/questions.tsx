8f4503c86ede83fc1f360339bc2869aedf046821
sanx111 <dudu-sanchez@hotmail.com>
2026-09-26 01:52:05 -0300

    feature(question): adicionados tipos de perguntas e placeholders na pagina

--- src/components/ui/questions.tsx
+++ src/components/ui/questions.tsx
@@ -1,4 +1,7 @@
 import { Button } from '@/components/ui/button';
+import { Progress } from '@/components/ui/progress';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { useState } from 'react';
 
 interface QuestionProps {
   perguntaAtual: number;
@@ -6,8 +9,10 @@ interface QuestionProps {
   progresso: number;
   titulo: string;
   subtitulo: string;
-  conteudo: React.ReactNode;
+  tipo: 'objetiva' | 'dissertativa' | 'quantitativa';
+  respostas?: string[];
   onProximo: () => void;
+  onVoltar: () => void;
 }
 
 export function Questions({
@@ -16,37 +21,100 @@ export function Questions({
   progresso,
   titulo,
   subtitulo,
-  conteudo,
+  tipo,
+  respostas = [],
   onProximo,
+  onVoltar,
 }: QuestionProps) {
+  const [texto, setTexto] = useState('');
   return (
     <div className="mx-auto my-auto flex w-full max-w-xl flex-col items-center justify-center gap-6 py-6">
-      <div className="w-full rounded-2xl border border-border bg-card p-5 shadow-sm">
-        <div className="text-center">
-          <p>
+      <div className="w-full space-y-4 rounded-2xl border border-border bg-card p-5 shadow-sm">
+        <header className="flex w-full flex-col items-center justify-center gap-4 px-6 py-1 sm:flex-row sm:items-start sm:justify-between">
+          <p className="text-2xl font-bold text-heading">Seção 1</p>
+          <p className="text-xl font-bold text-heading">
             Pergunta {perguntaAtual} de {totalPerguntas}
           </p>
-        </div>
-
-        <progress value={progresso} />
-
+        </header>
+        <Progress value={progresso} />
         <div className="bg-brand flex flex-col items-start justify-start gap-4 rounded-xl p-8 text-primary-foreground">
           <h2 className="text-xl font-bold">{titulo}</h2>
           <p>{subtitulo}</p>
         </div>
-
-        <div className="p-4 pt-6">{conteudo}</div>
-
-        <div className="flex w-full items-center justify-end rounded-2xl border border-border bg-background p-2">
+        {/* perguntas objetivas */}
+        {tipo === 'objetiva' && (
+          <RadioGroup>
+            {respostas.map((resposta, index) => (
+              <div
+                key={`op${String(index)}`}
+                className="flex cursor-pointer items-center space-x-2 rounded-lg border border-primary px-4 py-3 hover:bg-primary/5"
+              >
+                <RadioGroupItem value={resposta} id={`op${String(index)}`} />
+                <label
+                  htmlFor={`op${String(index)}`}
+                  className="flex-1 cursor-pointer text-primary"
+                >
+                  {resposta}
+                </label>
+              </div>
+            ))}
+          </RadioGroup>
+        )}
+        {/* perguntas dissertativas */}
+        {tipo === 'dissertativa' && (
+          <textarea
+            value={texto}
+            onChange={(e) => {
+              setTexto(e.target.value);
+            }}
+            placeholder="Digite sua resposta aqui..."
+            className="w-full rounded-lg border border-primary p-4 text-primary focus:ring-2 focus:ring-primary focus:outline-none"
+            rows={6}
+          />
+        )}
+        {/* perguntas quantitativas */}
+        {tipo === 'quantitativa' && (
+          <RadioGroup className="flex justify-center gap-2">
+            {respostas.map((resposta, index) => (
+              <div
+                key={`op${String(index)}`}
+                className="flex cursor-pointer items-center space-x-2 rounded-lg border border-primary px-4 py-3 hover:bg-primary/5"
+              >
+                <RadioGroupItem
+                  variant="caixa"
+                  value={resposta}
+                  id={`op${String(index)}`}
+                />
+                <label
+                  htmlFor={`op${String(index)}`}
+                  className="flex-1 cursor-pointer text-primary"
+                >
+                  {resposta}
+                </label>
+              </div>
+            ))}
+          </RadioGroup>
+        )}
+        <footer className="flex w-full items-center justify-between rounded-2xl border border-border bg-background p-2">
           <Button
-            className="bg-brand text-primary-foreground"
+            variant={'outline'}
+            className="text-brand"
+            onClick={() => {
+              onVoltar();
+            }}
+          >
+            Voltar
+          </Button>
+          <p>~tempo restante</p>
+          <Button
+            className="bg-brand font-bold text-primary-foreground"
             onClick={() => {
               onProximo();
             }}
           >
-            próximo
+            Avançar
           </Button>
-        </div>
+        </footer>
       </div>
     </div>
   );
--- src/features/form/formView.tsx
+++ src/features/form/formView.tsx
@@ -5,20 +5,21 @@ import { useState } from 'react';
 import { Questions } from '@/components/ui/questions';
 
 export function FormView() {
-  //const { t } = useTranslation(['Onboard_Quest']);
   const [perguntaAtual, setPerguntaAtual] = useState(1);
   const totalPerguntas = 3;
   const progresso = (perguntaAtual / totalPerguntas) * 100;
 
   return (
     <div className="flex min-h-svh flex-col overflow-hidden bg-background">
-      <header className="flex w-full items-center justify-between px-6 py-10">
+      <header className="flex w-full flex-col items-center justify-center gap-4 px-6 py-10 sm:flex-row sm:items-start sm:justify-between">
         <div>
           <p className="text-sm font-bold text-heading">João Silva</p>
-          <h1 className="text-3xl font-bold text-heading">FORM</h1>
+          <h1 className="text-center text-3xl font-bold text-heading sm:text-left">
+            FORM
+          </h1>
         </div>
 
-        <div className="flex items-center justify-end gap-1 rounded-2xl border border-border p-2 shadow-sm">
+        <div className="flex flex-wrap items-center justify-center gap-1 rounded-2xl border border-border p-2 shadow-sm sm:justify-end">
           <button className="text-brand p-2 transition-colors hover:opacity-80">
             <LayoutDashboard size={24} />
           </button>
@@ -35,20 +36,64 @@ export function FormView() {
           </button>
         </div>
       </header>
+
       <div className="flex flex-1 flex-col overflow-hidden p-6 lg:p-12">
-        <div className="flex flex-1 flex-col overflow-hidden p-6 lg:p-12">
+        {/* Todas as perguntas são placehoders hardcodadas e serão importadas do backend quando possivel*/}
+        {/* Pergunta objetiva */}
+        {perguntaAtual === 1 && (
           <Questions
             perguntaAtual={perguntaAtual}
             totalPerguntas={totalPerguntas}
             progresso={progresso}
             titulo="Pergunta 1"
             subtitulo="OBJETIVA"
-            conteudo={<p>respostas</p>}
+            tipo="objetiva"
+            respostas={['Resposta 1', 'Resposta 2', 'Resposta 3']}
             onProximo={() => {
               setPerguntaAtual(perguntaAtual + 1);
             }}
+            onVoltar={() => {
+              setPerguntaAtual(perguntaAtual - 1);
+            }}
           />
-        </div>
+        )}
+
+        {/* Pergunta dissertativa */}
+        {perguntaAtual === 2 && (
+          <Questions
+            perguntaAtual={perguntaAtual}
+            totalPerguntas={totalPerguntas}
+            progresso={progresso}
+            titulo="Pergunta 2"
+            subtitulo="DISSERTATIVA"
+            tipo="dissertativa"
+            onProximo={() => {
+              setPerguntaAtual(perguntaAtual + 1);
+            }}
+            onVoltar={() => {
+              setPerguntaAtual(perguntaAtual - 1);
+            }}
+          />
+        )}
+
+        {/* Pergunta quantitativa*/}
+        {perguntaAtual === 3 && (
+          <Questions
+            perguntaAtual={perguntaAtual}
+            totalPerguntas={totalPerguntas}
+            progresso={progresso}
+            titulo="Pergunta 3"
+            subtitulo="QUANTITATIVA"
+            tipo="quantitativa"
+            respostas={['1', '2', '3', '4', '5']}
+            onProximo={() => {
+              setPerguntaAtual(perguntaAtual + 1);
+            }}
+            onVoltar={() => {
+              setPerguntaAtual(perguntaAtual - 1);
+            }}
+          />
+        )}
       </div>
     </div>
   );
